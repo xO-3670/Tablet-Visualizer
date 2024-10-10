c839641782653318ac6375eaf7cef5bc289f46ed
xO-3670 <105218851+xO-3670@users.noreply.github.com>
2024-10-10 02:35:26 +0200

    initial commit

---
+++ Cursor.hpp
@@ -0,0 +1,38 @@
+#ifndef CURSOR_HPP
+#define CURSOR_HPP
+
+#include <SFML\Graphics.hpp>
+
+#include <vector>
+#include <algorithm>
+
+#include "TrailCircle.hpp"
+
+
+class Cursor : public sf::Drawable
+{
+public:
+    Cursor() = default;
+    Cursor(float_t cursorSize, uint32_t trailDensity, float_t sizeOfTrailCircles, float_t trailCirclesLifetime ,sf::Texture& cursorTexture, sf::Texture& cursorTrailTexture);
+    ~Cursor() = default;
+
+    void Update(sf::Vector2f cursorPosition);
+
+    const sf::Vector2f& GetPosition();
+
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states)const;
+
+private:
+    sf::CircleShape _CursorBody;
+
+    std::vector<TrailCircle> _TrailCircles;
+    sf::Texture*            _CursorTrailTexturePtr;
+
+    uint16_t _TrailDensity;
+    float_t  _TrailCirclesSize;
+    float_t  _TrailCirclesLifetime = 70.f;
+
+};
+
+#endif
